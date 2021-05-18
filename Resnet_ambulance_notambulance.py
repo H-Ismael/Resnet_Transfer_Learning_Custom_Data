4253db5ef1852b5cc9c614a661d729b473a5b825
H-Ismael <59347097+H-Ismael@users.noreply.github.com>
2021-05-18 17:59:56 +0100

    Update Resnet_ambulance_notambulance.py

--- Resnet_ambulance_notambulance.py
+++ Resnet_ambulance_notambulance.py
@@ -1,3 +1,84 @@
+import tensorflow as tf
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import Model 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, Dense, MaxPooling2D, Dropout, Flatten,GlobalAveragePooling2D
+
+
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import pandas as pd
+
+
+
+train_data_gen = ImageDataGenerator(rotation_range=50,
+                                    width_shift_range=0.2,
+                                    height_shift_range=0.2,
+                                    zoom_range=0.3,
+                                    horizontal_flip=True,
+                                    vertical_flip=True,
+                                    fill_mode='constant',
+                                    cval=0,
+                                    rescale=1./255)
+valid_data_gen = ImageDataGenerator(rotation_range=45,
+                                    width_shift_range=0.2,
+                                    height_shift_range=0.2,
+                                    zoom_range=0.3,
+                                    horizontal_flip=True,
+                                    vertical_flip=True,
+                                    fill_mode='constant',
+                                    cval=0,
+                                    rescale=1./255)
+
+test_data_gen = ImageDataGenerator(rescale=1./255)
+
+dataset_dir = os.path.join('D:/************/trashIR/Image-Classification-Transfer-Learning/', 'Dataset')
+
+Batch_size = 32
+img_h = 256
+img_w = 256
+num_classes=2
+
+classes = ['ambulance',
+            'notambulance',
+           ]
+
+SEED = 1234
+tf.random.set_seed(SEED) 
+
+training_dir = os.path.join(dataset_dir, 'training')
+train_gen = train_data_gen.flow_from_directory(training_dir,
+                                               target_size=(256, 256),
+                                               batch_size=Batch_size,
+                                               classes=classes,
+                                               class_mode='categorical',
+                                               shuffle=True,
+                                               seed=SEED)  # targets are directly converted into one-hot vectors
+
+
+valid_dir = os.path.join(dataset_dir, 'valid')
+valid_gen = valid_data_gen.flow_from_directory(valid_dir,
+                                           target_size=(256, 256),
+                                           batch_size=Batch_size, 
+                                           classes=classes,
+                                           class_mode='categorical',
+                                           shuffle=False,
+                                           seed=SEED)
+
+test_dir = os.path.join(dataset_dir, 'test')
+test_gen = test_data_gen.flow_from_directory(test_dir,
+                                             target_size=(256, 256),
+                                             batch_size=10, 
+                                             shuffle=False,
+                                             seed=SEED,
+                                             class_mode=None,
+                                             )
+
+
+ResNet_model = tf.keras.applications.ResNet152V2(weights='imagenet', include_top=False, input_shape=(img_h, img_w, 3))
+
 for layer in ResNet_model.layers[:-15]:
     layer.trainable = False
 
@@ -5,8 +86,50 @@ x = ResNet_model.output
 x = GlobalAveragePooling2D()(x)
 x = Flatten()(x)
 x = Dense(units=512, activation='relu')(x)
-x = Dropout(0.5)(x)
+x = Dropout(0.3)(x)
 x = Dense(units=700, activation='relu')(x)
 x = Dropout(0.5)(x)
-output  = Dense(units=2, activation='relu')(x)
+output  = Dense(units=2, activation='softmax')(x)
 model = Model(ResNet_model.input, output)
+model.summary()
+
+loss = tf.keras.losses.CategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer=optimizer, loss=loss, metrics= ['accuracy'])
+
+lrr = ReduceLROnPlateau(monitor='val_accuracy', 
+                        patience=3, 
+                        verbose=1, 
+                        factor=0.4, 
+                        min_lr=0.0001)
+
+
+callbacks = [lrr]
+
+STEP_SIZE_TRAIN=train_gen.n//train_gen.batch_size
+STEP_SIZE_VALID=valid_gen.n//valid_gen.batch_size
+transfer_learning_history = model.fit_generator(generator=train_gen,
+                   steps_per_epoch=STEP_SIZE_TRAIN,
+                   validation_data=valid_gen,
+                   validation_steps=STEP_SIZE_VALID,
+                   epochs=1,
+                  callbacks=callbacks,
+                  class_weight='auto',
+                  
+                    
+)
+
+model.save('M1')
+
+
+model.evaluate(test_gen, steps=STEP_SIZE_VALID,verbose=1)
+
+
+
+STEP_SIZE_TEST=valid_gen.n//valid_gen.batch_size
+test_gen.reset()
+pred=model.predict(valid_gen,
+steps=STEP_SIZE_TEST,
+verbose=1)
+
+predicted_class_indices=np.argmax(pred,axis=1)
